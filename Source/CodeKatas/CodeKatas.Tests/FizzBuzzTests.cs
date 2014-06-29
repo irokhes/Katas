7bee1641d4d119a552a2e7eb6cdbdde0d2fe8759
irokhes <jorgereyero@gmail.com>
2014-06-29 12:58:20 +0100

    Create visual studio project
    Start FizzBuzz kata

---
+++ Source/CodeKatas/CodeKatas.Tests/FizzBuzzTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace CodeKatas.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void When_getting_divisible_by_3()
+        {
+            //Arrange
+            var fizzBuzz = new FizzBuzz();
+
+            //Act
+            var result = fizzBuzz.Answer(3);
+
+            //Assert
+            Assert.AreEqual("Fizz", result);
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public string Answer(int number)
+        {
+            return number % 3 == 0 ? "Fizz" : number.ToString();
+        }
+    }
+}
